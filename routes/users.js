1e21849444180f813acbaa6e74be5a173ca2e9a3
Charles Wang <lwan256@uwo.ca>
2024-02-19 16:36:08 -0500

    finishing up crud process

--- routes/users.js
+++ routes/users.js
@@ -26,6 +26,28 @@ router.put("/:id", async(req,res)=>{
     }
 })
 
+router.delete("/:id",async(req,res)=>{
+    if(req.body.userId === req.params.id||req.user.isAdmin){
+        try{
+            const user = await User.deleteOne(req.params.id)
+            res.status(200).json("Account has been deleted")
+        }catch(error){
+            return res.status(500).json(500)
+        }
+    }else{
+        return res.status(403).json("You can delete only your account")
+    }
+})
+
+router.get("/:id", async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      const { password, updatedAt, ...other } = user._doc;
+      res.status(200).json(other);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 
 module.exports = router
